cd7cc7ba3c124e9f5f0db7f12afa66e3e3e4186d
im23b-busere <busere@bzz.ch>
2024-08-21 11:35:41 +0200

    some lint error fixed

--- door.py
+++ door.py
@@ -1,55 +1,88 @@
+"""Module for Door and DoorLock classes"""
+
+
 class Door:
+    """A class representing a door with a lock."""
+
     def __init__(self, ref2door_lock, base_color):
+        """Initialize a Door object.
+
+        Args:
+          ref2door_lock (DoorLock): A reference to a DoorLock object.
+          base_color (str): The color of the door.
+        """
         self._the_door_lock = ref2door_lock
         self.color = base_color
         self._door_is_open = False
         self._door_is_locked = False
 
     def open_the_door(self):
+        """Open the door if it's not locked."""
         if not self._door_is_locked:
             self._door_is_open = True
 
     def close_the_door(self):
+        """Close the door."""
         self._door_is_open = False
 
     def lock_the_door(self):
+        """Lock the door if it's closed."""
         if not self._door_is_open:
             self._door_is_locked = self._the_door_lock.lock()
 
     def unlock_the_door(self):
+        """Unlock the door if it's locked."""
         if self._door_is_locked:
             self._door_is_locked = self._the_door_lock.unlock()
 
     def test(self):
+        """Print the state of the door."""
         print(f'Door color: {self.color}, '
               f'Door open: {self._door_is_open}, '
               f'Door locked: {self._door_is_locked}')
 
     @property
     def door_is_open(self):
+        """Check if the door is open."""
         return self._door_is_open
 
     @property
     def door_is_locked(self):
+        """Check if the door is locked."""
         return self._door_is_locked
 
     @property
     def color(self):
+        """Get the color of the door."""
         return self._color
 
     @color.setter
     def color(self, new_color):
+        """Set a new color for the door."""
         self._color = new_color
 
 
 class DoorLock:
+    """A class representing a door lock."""
+
     def __init__(self):
+        """Initialize a DoorLock object."""
         print("A lock has been created")
 
     def lock(self):
+        """Lock the door.
+
+        Returns:
+          bool: True if the door is locked.
+        """
         return True
 
     def unlock(self):
+        """Unlock the door.
+
+        Returns:
+          bool: False if the door is unlocked.
+        """
         return False
 
 
