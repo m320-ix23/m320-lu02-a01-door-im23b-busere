fd35cd4e877052889476101fd767013460a2746d
im23b-busere <busere@bzz.ch>
2024-08-21 11:38:02 +0200

    some lint error fixed

--- door.py
+++ door.py
@@ -1,32 +1,63 @@
-"""Module for Door and DoorLock classes"""
-
-
 class Door:
-    """A class representing a door with a lock."""
-
     def __init__(self, ref2door_lock, base_color):
-        """Initialize a Door object.
-
-        Args:
-          ref2door_lock (DoorLock): A reference to a DoorLock object.
-          base_color (str): The color of the door.
-        """
         self._the_door_lock = ref2door_lock
         self.color = base_color
         self._door_is_open = False
         self._door_is_locked = False
 
     def open_the_door(self):
-        """Open the door if it's not locked."""
         if not self._door_is_locked:
             self._door_is_open = True
 
     def close_the_door(self):
-        """Close the door."""
         self._door_is_open = False
 
     def lock_the_door(self):
-        """Lock the door if it's closed."""
         if not self._door_is_open:
             self._door_is_locked = self._the_door_lock.lock()
 
+    def unlock_the_door(self):
+        if self._door_is_locked:
+            self._door_is_locked = self._the_door_lock.unlock()
+
+    def test(self):
+        print(f'Door color: {self.color}, '
+              f'Door open: {self._door_is_open}, '
+              f'Door locked: {self._door_is_locked}')
+
+    @property
+    def door_is_open(self):
+        return self._door_is_open
+
+    @property
+    def door_is_locked(self):
+        return self._door_is_locked
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, new_color):
+        self._color = new_color
+
+
+class DoorLock:
+    def __init__(self):
+        print("A lock has been created")
+
+    def lock(self):
+        return True
+
+    def unlock(self):
+        return False
+
+
+if __name__ == "__main__":
+    print("Test for Door object")
+    the_door_lock = DoorLock()
+    the_door = Door(the_door_lock, "green")
+    the_door.test()
+    print("-- Open the door")
+    the_door.open_the_door()
+    the_door.test()
